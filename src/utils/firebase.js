8bd90d39e09674b8a6a810dc6e742c3cc9b97bee
ak1103dev <ak.apichan1103@gmail.com>
2019-06-08 23:52:17 +0700

    :wrench: make firebase config

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: 'sanqian-f42cb.firebaseapp.com',
-  databaseURL: 'https://sanqian-f42cb.firebaseio.com',
-  projectId: 'sanqian-f42cb',
-  storageBucket: 'sanqian-f42cb.appspot.com',
-  messagingSenderId: '69469755450',
-  appId: '1:69469755450:web:1da77ebc6bec54b8',
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 
 const app = firebase.initializeApp(firebaseConfig)
