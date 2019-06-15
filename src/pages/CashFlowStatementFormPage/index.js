c4ad21c3c9f620b6472c637025d8d02cdc5f4e5e
ak1103dev <ak.apichan1103@gmail.com>
2019-06-16 00:36:44 +0700

    :construction: can add statements

--- src/pages/CashFlowStatementFormPage/index.js
+++ src/pages/CashFlowStatementFormPage/index.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { connect } from 'react-redux'
 import { Formik, Form, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
 import dayjs from 'dayjs'
@@ -7,21 +9,70 @@ import dayjs from 'dayjs'
 import Container from '../../components/Container'
 import { Label, Input, Error } from '../../components/Form'
 import Button from '../../components/Button'
+import firebase from '../../utils/firebase'
+
+const db = firebase.firestore()
 
 const CustomForm = styled(Form)``
 
 const Schema = Yup.object().shape({
   type: Yup.number().required('required'),
-  cost: Yup.string().required('required'),
+  cost: Yup.number()
+    .min(1, 'at least 1')
+    .required('required'),
   category: Yup.string().required('required'),
   date: Yup.string().required('required'),
   note: Yup.string(),
 })
 
-function CashFlowStatementFormPage() {
+function CashFlowStatementFormPage({ userId, history }) {
+  const [catetories, setCategories] = useState([
+    {
+      id: '',
+      name: '-- select category --',
+    },
+  ])
+
+  useEffect(() => {
+    fetchCategories()
+    // return () => {
+    //   effect
+    // };
+  }, [])
+
+  const fetchCategories = async () => {
+    const categories = [
+      {
+        id: '',
+        name: '-- select category --',
+      },
+    ]
+    const querySnapshot = await db
+      .collection('categories')
+      .where('userId', '==', userId)
+      .get()
+    querySnapshot.forEach(function(doc) {
+      categories.push({
+        id: doc.id,
+        ...doc.data(),
+      })
+    })
+    setCategories(categories)
+  }
+
+  const addStatement = async values => {
+    const data = {
+      ...values,
+      cost: values.cost * 10000,
+      date: new Date(values.date),
+      userId,
+    }
+    await db.collection('statements').add(data)
+    history.goBack()
+  }
+
   const onSubmit = values => {
-    // eslint-disable-next-line no-console
-    console.log(values)
+    addStatement(values)
   }
 
   return (
@@ -48,15 +99,17 @@ function CashFlowStatementFormPage() {
             <ErrorMessage name="type" component={Error} />
             <Label>
               Cost
-              <Input type="number" name="cost" placeholder="0.00" />
+              <Input type="number" name="cost" min={1} placeholder="0.00" />
             </Label>
             <ErrorMessage name="cost" component={Error} />
             <Label>
               Category
               <Input component="select" name="category">
-                <option value="red">Red</option>
-                <option value="green">Green</option>
-                <option value="blue">Blue</option>
+                {catetories.map(item => (
+                  <option key={item.id} value={item.id}>
+                    {item.name}
+                  </option>
+                ))}
               </Input>
             </Label>
             <ErrorMessage name="category" component={Error} />
@@ -79,4 +132,13 @@ function CashFlowStatementFormPage() {
   )
 }
 
-export default CashFlowStatementFormPage
+CashFlowStatementFormPage.propTypes = {
+  userId: PropTypes.string.isRequired,
+  history: PropTypes.object.isRequired,
+}
+
+const mapState = ({ user }) => ({
+  userId: user.user.uid,
+})
+
+export default connect(mapState)(CashFlowStatementFormPage)
