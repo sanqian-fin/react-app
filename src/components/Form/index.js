269051c5e12515c44afd28151ba346606f546b15
ak1103dev <ak.apichan1103@gmail.com>
2019-06-15 18:08:44 +0700

    :lipstick: mock cash-flow-statement/add

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,3 +1,24 @@
 import styled from 'styled-components'
+import { Field } from 'formik'
 
-export const TextInput = styled.input``
+export const Label = styled.label`
+  display: block;
+  margin: 10px 0;
+`
+
+export const Input = styled(Field)`
+  display: block;
+  font-size: 16px;
+  border-radius: 5px;
+  outline: none;
+  padding: 5px;
+  box-sizing: border-box;
+  margin-top: 5px;
+  width: 100%;
+  border: 1px solid ${props => props.theme.colors.lightGray};
+`
+
+export const Error = styled.div`
+  color: ${props => props.theme.colors.error};
+  margin: 10px 0;
+`
--- src/pages/CashFlowStatementFormPage/index.js
+++ src/pages/CashFlowStatementFormPage/index.js
@@ -1,7 +1,82 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Formik, Form, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+import dayjs from 'dayjs'
+
+import Container from '../../components/Container'
+import { Label, Input, Error } from '../../components/Form'
+import Button from '../../components/Button'
+
+const CustomForm = styled(Form)``
+
+const Schema = Yup.object().shape({
+  type: Yup.number().required('required'),
+  cost: Yup.string().required('required'),
+  category: Yup.string().required('required'),
+  date: Yup.string().required('required'),
+  note: Yup.string(),
+})
 
 function CashFlowStatementFormPage() {
-  return <div>page</div>
+  const onSubmit = values => {
+    // eslint-disable-next-line no-console
+    console.log(values)
+  }
+
+  return (
+    <Container>
+      <Formik
+        initialValues={{
+          type: -1,
+          cost: '',
+          category: '',
+          date: dayjs().format('YYYY-MM-DD'),
+        }}
+        validationSchema={Schema}
+        onSubmit={onSubmit}
+      >
+        {() => (
+          <CustomForm>
+            <Label>
+              Type
+              <Input component="select" name="type">
+                <option value={1}>Income</option>
+                <option value={-1}>Expense</option>
+              </Input>
+            </Label>
+            <ErrorMessage name="type" component={Error} />
+            <Label>
+              Cost
+              <Input type="number" name="cost" placeholder="0.00" />
+            </Label>
+            <ErrorMessage name="cost" component={Error} />
+            <Label>
+              Category
+              <Input component="select" name="category">
+                <option value="red">Red</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+              </Input>
+            </Label>
+            <ErrorMessage name="category" component={Error} />
+            <Label>
+              Date
+              <Input type="date" name="date" />
+            </Label>
+            <ErrorMessage name="date" component={Error} />
+            <Label>
+              Note
+              <Input component="textarea" name="note" />
+            </Label>
+            <Button className="primary full" type="submit">
+              save
+            </Button>
+          </CustomForm>
+        )}
+      </Formik>
+    </Container>
+  )
 }
 
 export default CashFlowStatementFormPage
